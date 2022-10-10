c936976f62e607b6e663cb5e7c6f4cdf2d25611c
Éderson Fernandes <ederson.fernandes@bempromotora.onmicrosoft.com>
2022-10-10 09:34:11 -0300

    Component with context

--- src/components/Widget.stories.tsx
+++ src/components/Widget.stories.tsx
@@ -1,9 +1,9 @@
 import { Meta, StoryObj } from '@storybook/react';
-import { Loading } from "./Loading";
+import { Widget } from "./Widget";
 
 export default {
-    title: 'Components/Loading',
-    component: Loading,
+    title: 'Widget',
+    component: Widget,
 } as Meta;
 
 export const Default: StoryObj = {}
